e379e5398aec7f6c209dabc03fde2d15f2281372
Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>
2024-05-10 19:09:55 +1200

    1.0.0-alpha.4 - setPostProcessor custom variables.

--- src/main/java/com/mclegoman/luminance/client/shaders/Shader.java
+++ src/main/java/com/mclegoman/luminance/client/shaders/Shader.java
@@ -11,7 +11,10 @@ import com.mclegoman.luminance.client.data.ClientData;
 import com.mclegoman.luminance.client.translation.Translation;
 import com.mclegoman.luminance.common.data.Data;
 import com.mclegoman.luminance.common.util.LogType;
+import net.minecraft.client.gl.Framebuffer;
 import net.minecraft.client.gl.PostEffectProcessor;
+import net.minecraft.client.texture.TextureManager;
+import net.minecraft.resource.ResourceManager;
 import net.minecraft.util.Identifier;
 
 public class Shader {
@@ -27,9 +30,12 @@ public class Shader {
 		return postProcessor;
 	}
 	public void setPostProcessor() {
+		setPostProcessor(ClientData.minecraft.getTextureManager(), ClientData.minecraft.getResourceManager(), ClientData.minecraft.getFramebuffer(), ClientData.minecraft.getWindow().getFramebufferWidth(), ClientData.minecraft.getWindow().getFramebufferHeight());
+	}
+	public void setPostProcessor(TextureManager textureManager, ResourceManager resourceManager, Framebuffer framebuffer, int width, int height) {
 		try {
-			this.postProcessor = new PostEffectProcessor(ClientData.minecraft.getTextureManager(), ClientData.minecraft.getResourceManager(), ClientData.minecraft.getFramebuffer(), shaderId);
-			this.postProcessor.setupDimensions(ClientData.minecraft.getWindow().getFramebufferWidth(), ClientData.minecraft.getWindow().getFramebufferHeight());
+			this.postProcessor = new PostEffectProcessor(textureManager, resourceManager, framebuffer, shaderId);
+			this.postProcessor.setupDimensions(width, height);
 		} catch (Exception error) {
 			Data.version.sendToLog(LogType.ERROR, Translation.getString("Failed to set post processor: {}", error));
 			if (this.postProcessor != null) this.postProcessor.close();
