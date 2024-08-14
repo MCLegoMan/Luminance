9697f6f37b4617f9f438c106f6f6d7ce36e15406
Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>
2024-08-14 18:42:34 +1200

    1.0.0-alpha - message overlay is now rendered in gamerenderer.

--- src/main/java/com/mclegoman/luminance/mixin/client/hud/InGameHudMixin.java
+++ src/main/java/com/mclegoman/luminance/mixin/client/hud/InGameHudMixin.java
@@ -10,18 +10,23 @@ package com.mclegoman.luminance.mixin.client.hud;
 import com.mclegoman.luminance.client.data.ClientData;
 import com.mclegoman.luminance.client.util.MessageOverlay;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.render.BufferBuilderStorage;
+import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.render.RenderTickCounter;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(priority = 0, value = InGameHud.class)
+@Mixin(priority = 0, value = GameRenderer.class)
 public abstract class InGameHudMixin {
-	@Inject(at = @At("RETURN"), method = "render")
-	private void luminance$render(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
+	@Shadow @Final private BufferBuilderStorage buffers;
+	@Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;render(Lnet/minecraft/client/gui/DrawContext;Lnet/minecraft/client/render/RenderTickCounter;)V"), method = "render")
+	private void luminance$render(RenderTickCounter tickCounter, boolean tick, CallbackInfo ci) {
 		if (!ClientData.minecraft.gameRenderer.isRenderingPanorama()) {
+			DrawContext context = new DrawContext(ClientData.minecraft, this.buffers.getEntityVertexConsumers());
 			float h = MessageOverlay.remaining - ClientData.minecraft.getRenderTickCounter().getTickDelta(true);
 			int l = (int) (h * 255.0F / 20.0F);
 			if (l > 255) l = 255;
