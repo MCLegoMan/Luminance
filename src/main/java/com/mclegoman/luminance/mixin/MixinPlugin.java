6f44303e45b2a55e8459afb1ced4746289ae2b82
Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>
2024-09-26 12:44:30 +1200

    removed fixes from mixin plugin

--- src/main/java/com/mclegoman/luminance/mixin/MixinPlugin.java
+++ src/main/java/com/mclegoman/luminance/mixin/MixinPlugin.java
@@ -37,28 +37,6 @@ public class MixinPlugin implements IMixinConfigPlugin {
 	public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
 		String mixin = mixinClassName.replaceFirst("com.mclegoman.luminance.mixin.", "");
 		switch (mixin) {
-			case "client.shaders.ShaderNamespaceFix" -> {
-				boolean isArchitecturyInstalled = Data.isModInstalled("architectury");
-				boolean isSatinInstalled = Data.isModInstalled("satin");
-				boolean isSoupInstalled = (Data.isModInstalled("souper_secret_settings") && !Data.isModInstalledVersionOrHigher("souper_secret_settings", "v1.0.9", true));
-				boolean isPerspectiveInstalled = (Data.isModInstalled("perspective") && !Data.isModInstalledVersionOrHigher("perspective", "1.3.0-alpha.7"));
-				List<String> modsInstalled = new ArrayList<>();
-				if (isArchitecturyInstalled) modsInstalled.add("architectury");
-				if (isSatinInstalled) modsInstalled.add("satin");
-				if (isSoupInstalled) modsInstalled.add("souper_secret_settings");
-				if (isPerspectiveInstalled) modsInstalled.add("perspective");
-				if (!modsInstalled.isEmpty()) Data.version.sendToLog(LogType.INFO, Translation.getString("Disabling {}: {}", mixin, modsInstalled));
-				return modsInstalled.isEmpty();
-			}
-			case "client.shaders.ShaderTextureNamespaceFix" -> {
-				boolean isSoupInstalled = (Data.isModInstalled("souper_secret_settings") && !Data.isModInstalledVersionOrHigher("souper_secret_settings", "v1.0.9", true));
-				boolean isPerspectiveInstalled = Data.isModInstalled("perspective") && !Data.isModInstalledVersionOrHigher("perspective", "1.3.0-alpha.7");
-				List<String> modsInstalled = new ArrayList<>();
-				if (isSoupInstalled) modsInstalled.add("souper_secret_settings");
-				if (isPerspectiveInstalled) modsInstalled.add("perspective");
-				if (!modsInstalled.isEmpty()) Data.version.sendToLog(LogType.INFO, Translation.getString("Disabling {}: {}", mixin, modsInstalled));
-				return modsInstalled.isEmpty();
-			}
 			case "compat.modmenu.FabricModMixin", "compat.modmenu.ModsScreenMixin" -> {
 				boolean isModMenuInstalled = Data.isModInstalled("modmenu");
 				List<String> modsInstalled = new ArrayList<>();
