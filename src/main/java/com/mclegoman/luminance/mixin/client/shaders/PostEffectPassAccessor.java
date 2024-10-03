365a40fd2242419c9c59748411cfddbde0ccddf7
Nettakrim <106409257+Nettakrim@users.noreply.github.com>
2024-10-04 00:20:42 +0100

    accessors werent enough to determine what uniforms a program has available

--- src/main/java/com/mclegoman/luminance/mixin/client/shaders/PostEffectPassAccessor.java
+++ src/main/java/com/mclegoman/luminance/mixin/client/shaders/PostEffectPassAccessor.java
@@ -2,6 +2,7 @@ package com.mclegoman.luminance.mixin.client.shaders;
 
 import net.minecraft.client.gl.PostEffectPass;
 import net.minecraft.client.gl.PostEffectPipeline;
+import net.minecraft.client.gl.ShaderProgram;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
@@ -12,6 +13,9 @@ public interface PostEffectPassAccessor {
     @Accessor("id")
     String getID();
 
+    @Accessor("program")
+    ShaderProgram getProgram();
+
     @Accessor("uniforms")
-    List<PostEffectPipeline.Uniform> getUniforms();
+    List<PostEffectPipeline.Uniform> getUniformOverrides();
 }
