7177ae51b2cdee22cfc7f900d0ebdc36bf8e8bcf
Nettakrim <106409257+Nettakrim@users.noreply.github.com>
2024-10-09 00:19:00 +0100

    updated to 1.21.2-pre1 (woo shader uniform fix)

--- src/main/java/com/mclegoman/luminance/mixin/client/shaders/ShaderProgramMixin.java
+++ src/main/java/com/mclegoman/luminance/mixin/client/shaders/ShaderProgramMixin.java
@@ -3,6 +3,7 @@ package com.mclegoman.luminance.mixin.client.shaders;
 import com.mclegoman.luminance.client.shaders.ShaderProgramInterface;
 import net.minecraft.client.gl.GlUniform;
 import net.minecraft.client.gl.ShaderProgram;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -12,11 +13,12 @@ import java.util.List;
 
 @Mixin(ShaderProgram.class)
 public class ShaderProgramMixin implements ShaderProgramInterface {
+    @Final
     @Shadow
-    private List<GlUniform> uniforms = null;
+    private List<GlUniform> uniforms;
 
     @Unique
-    private List<String> uniformNames = null;
+    private List<String> uniformNames;
 
     @Override public List<String> luminance$getUniformNames() {
         if (uniformNames == null) {
